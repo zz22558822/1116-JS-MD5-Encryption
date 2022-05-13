6ce07e17902a927933c7169fb5808acbe02733cd
Chek <79972532+zz22558822@users.noreply.github.com>
2022-05-13 13:45:43 +0800

    更新特別算法2

--- js/all.js
+++ js/all.js
@@ -1,6 +1,7 @@
 const originText = document.querySelector('.originText');
 const MD5Text = document.querySelector('.MD5Text');
 const MD5TextSpecial = document.querySelector('.MD5Text-special');
+const MD5TextSpecial2 = document.querySelector('.MD5Text-special2');
 const capital = document.querySelector('#capital');
 const btn = document.querySelector('.btn');
 const Pbox = document.querySelector('.prompt-box');
@@ -21,6 +22,18 @@ function MD5Special() {
   MD5TextSpecial.innerHTML = `特別算法 :  ${MD5Sa}`;
 }
 
+// 特別算法2函式
+function MD5Special2() {
+  let MD5S = md5(originText.value).toUpperCase();
+  MD5S = md5(MD5S.substring(0, 3) + MD5S.substring(29, 32)).toUpperCase();
+
+  MD5S = md5(MD5S.substring(0, 3) + MD5S.substring(29, 32)).toUpperCase();
+  MD5S = md5(MD5S.substring(0, 3) + MD5S.substring(29, 32)).toUpperCase();
+  
+  const MD5Sa = MD5S.substring(0, 3) + MD5S.substring(29, 32);
+  MD5TextSpecial2.innerHTML = `特別算法 :  ${MD5Sa}`;
+}
+
 // 執行加密主程序
 function start() {
   if (originText.value === '') {
@@ -35,11 +48,15 @@ function start() {
     MD5Text.value = MD5Num;
     // 特別算法
     MD5Special();
+    // 特別算法2
+    MD5Special2();
   } else if (capital.checked === true) {
     const MD5Num = md5(originText.value).toUpperCase();
     MD5Text.value = MD5Num;
     // 特別算法
     MD5Special();
+    // 特別算法2
+    MD5Special2();
   }
 }
 
